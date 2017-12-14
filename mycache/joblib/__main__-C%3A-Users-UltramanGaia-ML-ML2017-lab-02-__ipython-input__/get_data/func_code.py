a83899df9c288a04fb7047dbcbdf1d2eafb28e70
UltramanGaia <3213359017@qq.com>
2017-12-15 00:12:30 +0800

    interesting

---
+++ mycache/joblib/__main__-C%3A-Users-UltramanGaia-ML-ML2017-lab-02-__ipython-input__/get_data/func_code.py
@@ -0,0 +1,12 @@
+# first line: 1
+@mem.cache
+def get_data():
+    train_X, train_y = load_svmlight_file('data/a9a', n_features=123)
+    test_X, test_y = load_svmlight_file('data/a9a.t', n_features=123)
+
+    train_y =  train_y.reshape(train_y.shape[0],1)
+    test_y =  test_y.reshape(test_y.shape[0],1)
+
+    train_y[train_y == -1] = 0
+    test_y[test_y == -1] = 0
+    return train_X, test_X, train_y, test_y
